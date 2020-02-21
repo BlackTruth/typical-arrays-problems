32f69226a8c1987035e3945b89eb255b04b0b60c
Kanstantsin Kulahin <blacktruth1991@gmail.com>
2020-02-21 09:29:03 +0300

    itit: typical-arrays-problem task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) return 0;
+    else {
+        let min = array[0];
+        for (let i of array) if (i < min) min = i;
+        return min;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) return 0;
+    else {
+        let max = array[0];
+        for (let i of array) if (i > max) max = i;
+        return max;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) return 0;
+    else {
+        let sum = 0;
+        for (let i of array) sum += i;
+        return sum / array.length;
+    }
+};
